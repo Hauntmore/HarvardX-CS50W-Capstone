f74a5f59bd633eb6431cc0492d0a8cad660668f7
Hauntmore <kevinle3212@gmail.com>
2024-07-15 16:49:48 -0700

    chore, fix: add a pylint comment to ignore a formatting issue, create comments that add detail to specific code.

--- capstone/manage.py
+++ capstone/manage.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python
+
 """
 Django's command-line utility for administrative tasks.
 """
+
 import os
 import sys
 
 
 def main():
     """
-    Run administrative tasks.
+    Assist in running administrative tasks.
     """
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "capstone.settings")
+
     try:
         # pylint: disable=import-outside-toplevel
+
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
@@ -20,6 +25,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
     execute_from_command_line(sys.argv)
 
 
