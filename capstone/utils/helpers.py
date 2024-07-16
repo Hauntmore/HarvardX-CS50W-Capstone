b2942f1ec67b50fb9233b0eb0cadf023b79be747
Hauntmore <kevinle3212@gmail.com>
2024-07-15 18:34:25 -0700

    chore, fix: add doctypes for the module and load_configuration_file() function, add in a default path to the function for the parameter.

--- capstone/utils/helpers.py
+++ capstone/utils/helpers.py
@@ -1,11 +1,32 @@
+"""
+Reusable utility functions and helper methods to assist in executing various common operations and tasks throughout the project.
+"""
+
 import json
 import os
 
 from django.conf import settings
 
 
-def load_configuration_file():
-    path = os.path.join(settings.BASE_DIR, "..", "config.json")
+def load_configuration_file(
+    path: str = os.path.join(settings.BASE_DIR, "..", "config.json")
+):
+    """
+    A function that loads the config.json file and return its properties.
+
+    Args:
+        path (str): The path to the configuration file.
+
+    Returns:
+        dict: A dictionary containing the configuration data parsed from the file.
+
+    Raises:
+        FileNotFoundError: If the specified path does not exist.
+        IOError: If there is an issue reading the file.
+
+    Example:
+        >>> contents = load_configuration_file('./path/to/config.json')
+    """
 
     with open(path, "r", encoding="UTF-8") as file:
         contents = json.load(file)
