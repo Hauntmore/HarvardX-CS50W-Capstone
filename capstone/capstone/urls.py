3809bd1c5d9b89d22fbf20a0bb1f864d9c2f6ebd
Hauntmore <kevinle3212@gmail.com>
2024-07-15 17:03:03 -0700

    chore, feat: update informational comments to describe the file, and add an if statement to serve media and static files when settings define 'DEBUG = True'.

--- capstone/capstone/urls.py
+++ capstone/capstone/urls.py
@@ -1,23 +1,20 @@
 """
-URL configuration for capstone project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+The Uniform Resource Locator (URL) configuration for the Capstone project. This file defines routes that map specific URLs to their corresponding views. For more information on this file please see:
+https://docs.djangoproject.com/en/5.0/topics/http/urls/
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("authentication/", include("apps.authentication.urls")),
+    path("event/", include("apps.event.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
