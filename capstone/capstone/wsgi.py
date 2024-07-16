29ff8f4103298e4c1b783cc01e0ac713ad4eaddc
Hauntmore <kevinle3212@gmail.com>
2024-07-15 17:06:59 -0700

    style: add descriptive module string doctype to describe the file.

--- capstone/capstone/wsgi.py
+++ capstone/capstone/wsgi.py
@@ -1,9 +1,5 @@
 """
-WSGI configuration for capstone project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
+The Web Server Gateway Interface (WSGI) configuration for Capstone. This file exposes the WSGI callable as a module-level variable named ``application``. For more information on this file, see:
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
