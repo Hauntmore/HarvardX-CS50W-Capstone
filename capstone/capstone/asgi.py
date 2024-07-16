c55f32f8d67d191631f44538d5b52360f6183372
Hauntmore <kevinle3212@gmail.com>
2024-07-15 17:07:47 -0700

    style: add descriptive module string doctype to describe the file.

--- capstone/capstone/asgi.py
+++ capstone/capstone/asgi.py
@@ -1,9 +1,5 @@
 """
-ASGI configuration for capstone project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
+The asynchronous Server Gateway Interface (ASGI) config for Capstone. This module exposes the ASGI callable as a module-level variable named ``application``. For more information on this file, see:
 https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
